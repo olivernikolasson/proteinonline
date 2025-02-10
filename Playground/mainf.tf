5e04b1a9c874bb877efbeb259c1501e3a3e963c9
Oliver Nikolasson <oliver.nikolasson@outlook.com>
2025-02-10 09:34:40 +0100

    Added playground and new workspace

---
+++ Playground/mainf.tf
@@ -0,0 +1,75 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=4.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  resource_provider_registrations = "none"
+  features {
+
+  }
+}
+
+
+resource "azurerm_resource_group" "Playground" {
+  name     = "Playground"
+  location = "West Europe"
+  tags = {
+    Env         = "Playground"
+    Provisioned = "Terraform"
+
+  }
+
+}
+
+resource "azurerm_virtual_network" "PlaygroundNet" {
+  name                = "Playground-net"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.Playground.location
+  resource_group_name = azurerm_resource_group.Playground.name
+
+}
+
+resource "azurerm_subnet" "PlaygroundSub" {
+  name                 = "PlaygroundSub"
+  resource_group_name  = azurerm_resource_group.Playground.name
+  virtual_network_name = azurerm_virtual_network.PlaygroundNet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "PlaygroundNic" {
+  name                = "playground-Nic"
+  resource_group_name = azurerm_resource_group.Playground.name
+  location            = azurerm_resource_group.Playground.location
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.PlaygroundSub.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+
+resource "azurerm_linux_virtual_machine" "Ubuntu" {
+  resource_group_name   = azurerm_resource_group.Playground.name
+  name                  = "ubuntuground1"
+  location              = azurerm_resource_group.Playground.location
+  admin_username        = "bryggan"
+  size                  = "Standard_F2"
+network_interface_ids = [ 
+    azurerm_network_interface.PlaygroundNic.id
+ ]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
